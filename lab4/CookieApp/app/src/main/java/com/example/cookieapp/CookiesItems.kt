1c78439a38bbef9934281db33321cda29ba11394
RLafleet <maksim.nazarov@ispring.institute>
2024-11-28 12:55:13 +0300

    added new upgrading items, also added getter for images with unique ids pictures. Also add new items in ClickerState

--- lab4/CookieApp/app/src/main/java/com/example/cookieapp/CookiesItems.kt
+++ lab4/CookieApp/app/src/main/java/com/example/cookieapp/CookiesItems.kt
@@ -7,9 +7,19 @@ data class ShopItem(
     val passiveRate: Int = 0,
     val maxLevel: Int = 5,
     val currentLevel: Int = 0
-)
+) {
+    val imageResId: Int
+        get() {
+            return when (name) {
+                "Cursor" -> R.drawable.cursor_64px
+                "Grandma" -> R.drawable.grandma_new
+                "Farm" -> R.drawable.farm
+                "Mine" -> R.drawable.mine_new
+                else -> R.drawable.cookie
+            }
+        }
+}
 
-// добавить доступность по условиям
 data class ClickerState(
     val cookieCount: Int = 0,
     val clickMultiplier: Int = 1,
@@ -17,8 +27,12 @@ data class ClickerState(
     val shopItems: List<ShopItem> = listOf(
         ShopItem("Double Cookies", 10, multiplier = 2),
         ShopItem("Cookie Generator", 25, passiveRate = 1),
-        ShopItem("Advanced Generator", 100, passiveRate = 5)
+        ShopItem("Advanced Generator", 100, passiveRate = 5),
+        ShopItem("Cursor", 5, multiplier = 1),
+        ShopItem("Grandma", 20, passiveRate = 2),
+        ShopItem("Farm", 50, passiveRate = 5),
+        ShopItem("Mine", 150, passiveRate = 10)
     ),
-    val elapsedTime: String = "00:00:00",
+    val elapsedTime: String = "0:00",
     val averageSpeed: Int = 0,
 )

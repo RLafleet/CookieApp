be18a6dfb0ffa56b010634c89c5975414fd5b8bf
RLafleet <maksim.nazarov@ispring.institute>
2024-11-29 11:36:57 +0300

    added observer for observe toasts

--- lab4/CookieApp/app/src/main/java/com/example/cookieapp/MainActivity.kt
+++ lab4/CookieApp/app/src/main/java/com/example/cookieapp/MainActivity.kt
@@ -1,7 +1,16 @@
 package com.example.cookieapp
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +21,23 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.main, ClickerFragment())
                 .commit()
+        }
+
+        observeToasts()
+    }
+
+    private val viewModel: ClickerViewModel by viewModels()
+
+    private fun observeToasts() {
+        lifecycleScope.launch {
+            // Наблюдаем за потоком тостов
+            viewModel.toastFlow.collectLatest { message ->
+                Log.d("MainActivity", "Received toast message: $message")
+                if (message.isNotEmpty()) {
+                    // Показываем тост
+                    Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
